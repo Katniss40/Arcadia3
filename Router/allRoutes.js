3ec9b9f879bb5f0e86ce797c49a96e6e83aaedec
Bourdeloux Corinne <bourdeloux.corinne@orange.fr>
2024-10-10 21:50:03 +0200

    creation des nouvelles pages et ajout sur le routage

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,12 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/services", "services", "/pages/services.html"),
+    new Route("/jungle", "jungle", "/pages/jungle.html"),
+    new Route("/marais", "marais", "/pages/marais.html"),
+    new Route("/savane", "savane", "/pages/savane.html"),
+    new Route("/habitats", "habitats", "/pages/habitats.html"),
+    new Route("/connexion", "connexion", "/pages/connexion.html"),
 
 ];
 
