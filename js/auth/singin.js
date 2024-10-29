e3470112ebd3292768964f4550364c0104eb106b
katniss <bourdeloux.corinne@orange.fr>
2024-10-29 07:54:50 +0100

    test token

--- js/auth/singin.js
+++ js/auth/singin.js
@@ -8,12 +8,12 @@ function checkCredentials() {
 
     // Ici il faudra appeler l'API pur vérifier les credentials en BDD
      
-    if(InputEmail.value == "bourdeloux.corinne@orange.fr" && InputPassword.value == "12345") {
+   if(InputEmail.value == "administrateur@arcadia.fr" && InputPassword.value == "12345") {
         alert("Vous êtes connecté");
 
         // Il faudra récupérer le vrai token
-        const token = "svlsijdmziqefhiqshvmxu";
-
+        const token = "mise en cookie de la connexion du Zoo arcadia";
+        setToken(token);
         //Placer ce token en cookie
 
         window.location.replace("/");
@@ -23,3 +23,35 @@ function checkCredentials() {
         InputPassword.classList.add("is-invalid");
     }
 }
+
+function isConnected() {
+    if(getToken() == null || getToken == undefined) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+
+
+/*const togglePassword =
+document.querySelector('#togglePassword');
+const password = document.querySelector('#id_password');
+
+togglePassword.addEventListener('clik', function () {
+    
+    const type = password.getAttribute('type') === 'password'? 'text' : 'password';
+    password.setAttribute('type', type);
+
+    if (togglePassword.scroll.match("site icone ouvert" )) {
+        tooglePassword.src = "site icone fermer"
+    }
+    else {
+        togglePassword.src = "site icone fermer"
+    }
+
+    });
+
+   // this.textContent = type == 'password' ? 'Afficher' : 'Masquer';
+//});*/
